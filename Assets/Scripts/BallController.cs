ea9fb5c7071f28d96c509c9b4da168f0668bc77e
taliafr <78625077+taliafr@users.noreply.github.com>
2023-11-14 15:50:07 +0100

    ball rolling

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public float acceleration = 10;
+    //public Transform ballCamera;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        float moveLeftRight = Input.GetAxis ("Horizontal");
+        float moveForwardBack = Input.GetAxis ("Vertical");
+        Vector3 xAcceleration = new Vector3(1, 0, 0) * moveLeftRight * Time.deltaTime * acceleration;
+        Vector3 zAceleration = new Vector3(0, 0, 1) * moveForwardBack * Time.deltaTime * acceleration;
+        rb.velocity += xAcceleration + zAceleration;
+    }
+}
