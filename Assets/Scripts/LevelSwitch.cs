28eb6ba9ef38c6b77224f69e72ca8017dc8ac2ad
taliafr <78625077+taliafr@users.noreply.github.com>
2023-12-10 22:03:49 +0100

    levelswitch

--- Assets/Scripts/LevelSwitch.cs
+++ Assets/Scripts/LevelSwitch.cs
@@ -1,31 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelSwitch : MonoBehaviour
 {
-    public Dropdown dropdown;
+    public TMP_Dropdown dropdown;
 
     public void ChangeScene(string sceneName)
     {
-        GameObject dropdownObject = GameObject.Find("DifficultyDropdown");
+        string selectedDifficulty = "";
 
-        string selectedDifficulty = "Easy";
-
-        if (dropdownObject != null)
-        {
-            // Get the Dropdown component from the GameObject
-            dropdown = dropdownObject.GetComponent<Dropdown>();
-
-            // Check if the Dropdown component is found
-            if (dropdown != null)
-            {
-                int selectedIndex = dropdown.value;
-                selectedDifficulty = dropdown.options[selectedIndex].text;
-            }
-        }
+        int selectedValue = dropdown.value;
+        selectedDifficulty = dropdown.options[selectedValue].text;
         
         SceneManager.LoadScene(sceneName + selectedDifficulty);
     }
